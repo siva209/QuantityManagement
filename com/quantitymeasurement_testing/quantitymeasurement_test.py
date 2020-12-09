c0fbb67a17a59b7bd20912bac8f7b5e77d1c2d93
siva209 <sivanarayanayeruva209@gmail.com>
2020-12-09 15:53:49 +0530

    [Added:test case for check feet value is not none]

--- com/quantitymeasurement_testing/quantitymeasurement_test.py
+++ com/quantitymeasurement_testing/quantitymeasurement_test.py
@@ -12,9 +12,14 @@ def test_givenTwoFeetValue_WhenCompared_ShouldReturnTrue():
     assert first_feet == second_feet
 
 
-# Test_case2 : Comparing Two instance feet variable
+# test_case2 : Comparing Two instance feet variable
 def test_givenTwoFeetValueInstanceVariable_WhenCompared_ShouldReturnTrue():
     first_feet = Feet(0.0)
     second_feet = first_feet
     assert first_feet == second_feet
 
+
+# test_Case3 : Comparing one feet value should return false when None
+def test_givenOneFeetValue_WhenComparedIfNotNone_ShouldReturnTrue():
+    first_feet = Feet(0.0)
+    assert first_feet is not None
