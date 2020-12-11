52c8c0535d7a24073a9612fec733d38af63495f5
siva209 <sivanarayanayeruva209@gmail.com>
2020-12-11 16:58:52 +0530

    [Added:feet class]

--- com/bridgelabz/quantitymeasurement/Feet.py
+++ com/bridgelabz/quantitymeasurement/Feet.py
@@ -1,3 +1,6 @@
+from com.bridgelabz.quantitymeasurement.Yard import Yard
+
+
 class Feet:
 
     def __init__(self, feet):
@@ -6,5 +9,8 @@ class Feet:
     def __eq__(self, other):
         if isinstance(other, Feet):
             if self.feet == other.feet:
+                return True
+        if isinstance(other, Yard):
+            if self.feet == other.yard * 3.0:
                 return True
         return False
