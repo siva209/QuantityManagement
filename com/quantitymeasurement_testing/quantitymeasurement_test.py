2c0695108706730c3303a573ec50fd43c9d138e2
siva209 <sivanarayanayeruva209@gmail.com>
2020-12-11 17:10:38 +0530

    [Added:test case for comparing two feet values]

--- com/quantitymeasurement_testing/quantitymeasurement_test.py
+++ com/quantitymeasurement_testing/quantitymeasurement_test.py
@@ -1,3 +1,4 @@
+from com.bridgelabz.quantitymeasurement.Feet import Feet
 from com.bridgelabz.quantitymeasurement.QuantityMeasurment import QuantityMeasurement, Length
 
 import pytest
@@ -8,4 +9,11 @@ import pytest
 def test_givenZeroFtandZeroFt_WhenCompared_ShouldReturnTrue():
     first_feet = QuantityMeasurement(Length.FEET, 0.0)
     second_feet = QuantityMeasurement(Length.FEET, 0.0)
+    assert first_feet == second_feet
+
+
+# case2 : comparing Two instance feet variable
+def test_givenTwoFeetValueInstanceVariable_WhenCompared_ShouldReturnTrue():
+    first_feet = Feet(0.0)
+    second_feet = first_feet
     assert first_feet == second_feet
