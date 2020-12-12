5fb44d7dd4ea003eaf046a70294809bf70de7249
siva209 <sivanarayanayeruva209@gmail.com>
2020-12-12 07:08:01 +0530

    [Added:Refactor of code]

--- com/bridgelabz/quantitymeasurement/QuantityMeasurment.py
+++ com/bridgelabz/quantitymeasurement/QuantityMeasurment.py
@@ -9,10 +9,15 @@ class QuantityMeasurement:
     def __eq__(self, other):
         if isinstance(other, QuantityMeasurement):
             return self.unit == other.unit and other.length == self.length
+
+    def compareto(self, other):
         if isinstance(self.unit, Length) and isinstance(other.unit, Length):
-            if Length.convert(self.unit, self.length) == Length.convert(other.unit, other.length):
-                return True
+            return Length.convert(self.unit, self.length) == Length.convert(other.unit, other.length)
+
         return False
+
+    def addition(self, other):
+        return Length.convert(self.unit, self.length) + Length.convert(other.unit, other.length)
 
 
 class Length(enum.Enum):
@@ -20,6 +25,9 @@ class Length(enum.Enum):
     YARD = 36.0
     INCH = 1.0
     CM = 0.4
+
+    def __init__(self, unit):
+        self.unit = unit
 
     def convert(self, length):
         return self.unit * length
